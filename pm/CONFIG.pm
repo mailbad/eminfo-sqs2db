51688696e4c7bb4f2ee25e72d7066dc45bebfa00
Guangzheng Zhang <zhang.elinks@gmail.com>
2013-10-07 22:38:47 +0800

    add CONFIG.pm HTTPSQS.pm

---
+++ pm/CONFIG.pm
@@ -0,0 +1,44 @@
+package CONFIG;
+# use Smart::Comments;
+
+$| = 1;
+
+# Read value by [section].key 
+# Example: 	print &conf_read('db','user');
+#
+sub conf_read {
+  my ($section, $key, $file) = @_;
+  return undef if (!$section || !$key);
+  $file = "/usr/local/eminfo-sqs2db/conf/sqs2db.ini" if !$file;
+
+  ### file: $file
+  ### section: $section
+  ### key: $key
+
+  unless (open FH, "<$file") {
+	return undef;
+  }
+
+  my $result = '';
+  my $flag = 0;
+  while (<FH>) {
+	if (m/\A\s*\[\s*($section)\s*\]\s*\Z/) {
+		$flag = 1;
+		next;
+	}
+ 	if (m/\A\s*\[\s*(\w+)\s*\]\s*\Z/) {
+		last if $flag == 1;
+	}
+	if (m/\A\s*$key\s*=\s*(.+)\s*\Z/) {
+		if ($flag == 1) {
+			$result = $1;
+			last;
+		}
+	}
+  }
+
+  ### result: $result
+  return $result;
+}
+
+1;
---
+++ pm/HTTPSQS.pm
@@ -0,0 +1,60 @@
+package HTTPSQS;
+use IO::Socket::SSL qw(debug0);   # debug0 ~ debug30
+#use Smart::Comments;
+
+$| = 1;
+
+
+#my %config = ( host=>'127.0.0.1', port=>'1218' );
+#my $result = &sqs_get("/?charset=utf-8&name=eminfo_postdata&auth=123qaz&opt=get", %config);
+#print "$result\n";
+
+
+# Get message from httpsqs
+# Usage: 	&sqs_get( {request_string}  %{sqs_server_conf} )
+# Example:	my %config=( host=>'127.0.0.1', port=>'1218' );
+#		&sqs_get("/?charset=utf-8&name=eminfo_postdata&auth=123qaz&opt=get", %config);
+
+sub sqs_get {
+  my $query = shift;
+  my %sqs_conf = @_;
+  ### $query
+  ### %sqs_conf
+
+  return undef if (!%sqs_conf || !$query);
+
+  my $result = '';
+
+  my $connect = IO::Socket::SSL->new(
+	'PeerAddr'		=> $sqs_conf{'host'} || '127.0.0.1',
+	'PeerPort'		=> $sqs_conf{'port'} || 1218,
+	'SSL_verify_mode'	=> 'SSL_VERIFY_NONE',
+	#'SSL_ca_path'		=> '/etc/ssl/certs',
+  );
+  unless ($connect) {
+	print "$!:$SSL_ERROR\n";
+	close $connect;
+	return undef;
+  }
+
+  my $request = "GET $query HTTP/1.1\r\n";
+  $request .= "Host: $sqs_conf{'host'}\r\n";
+  $request .= "Connection: close\r\n";
+  $request .= "\r\n" ;
+  ### $request
+
+  print $connect $request;
+  my $flag_header_end = 0;
+  while (<$connect>) {
+	if ($flag_header_end == 0) {
+		$flag_header_end = 1 if (/\A\s*\Z/);
+		next;
+	} else {
+		$result .= $_;
+	}
+  }
+
+  return $result;
+}
+
+1;
