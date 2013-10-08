7d3f30cf0a9451ebb4f8cb66209c801a31e62323
Guangzheng Zhang <zhang.elinks@gmail.com>
2013-10-08 22:09:26 +0800

    almost finished 0.1 version

---
+++ pm/DB.pm
@@ -0,0 +1,103 @@
+package DB;
+use DBI;
+use XML::Simple;
+use Smart::Comments;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw/db_update/;
+our @EXPORT_OK = qw/db_update/;
+
+$DB::VERSION = '1.0.0';
+
+
+$| = 1;
+
+my $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!-- eminfo postdata start --><eminfo_postdata><eminfo_id>752148030</eminfo_id><eminfo_gname>dev_bbklab.net</eminfo_gname><eminfo_jobid>sKR5Bmyr</eminfo_jobid><eminfo_jobts>1381128986</eminfo_jobts><eminfo_plugin_config><basic><name>test</name><enable>yes</enable><comment>Just a Test Plugin</comment><frequency>3min</frequency><exec_tmout>1min</exec_tmout><maxerr_times>1</maxerr_times><take_snapshot_type> crit warn unkn tmout </take_snapshot_type><mail_notify_type> crit warn unkn tmout </mail_notify_type><post_notify_type> crit warn unkn succ tmout </post_notify_type><mail_receviers>zhangguangzheng\@eyou.net</mail_receviers><attach_snap_mail>yes</attach_snap_mail><auto_handle_type>none </auto_handle_type><auto_handler>default_handler</auto_handler><debug>yes</debug></basic><userdef><user_defconf> test config value here</user_defconf></userdef></eminfo_plugin_config><eminfo_plugin_result><level>warn</level><type>str</type><body><title>summary </title><summary>title </summary><line size=\"10\" color=\"green\">green line</line><line size=\"8\" color=\"red\">red line</line><line size=\"9\">new line </line></body></eminfo_plugin_result><eminfo_autohandle_result><line size=\"38\">auto handle is disabled. Nothing to do</line></eminfo_autohandle_result><additional></additional></eminfo_postdata><!-- eminfo postdata end -->";
+my %dbconn=( dbhost=>'127.0.0.1', dbport=>'3306', database=>'eminfo', dbuser=>'eminfo', dbpass=>'eminfo' );
+&db_update('postlog',$xml,%dbconn);
+
+# Update message in db-backend
+# Usage: 	&db_update( {xml_text}  %{db_server_conf} )
+# Example:	my %dbconn=( dbhost=>'127.0.0.1', dbport=>'3306', database=>'eminfo', dbuser=>'eminfo', dbpass=>'eminfo' );
+# 		&db_update('postlog',"{xml postlog content}", %dbconn);
+#
+sub db_update {
+  my $table = shift;
+  my $text = shift;
+  my %db_conf = @_;
+  ### $table
+  ### $text
+  ### %db_conf
+
+  return undef if (!%db_conf || !$text || !$table);
+  return undef if ($table ne 'heartbeat' && $table ne 'postlog');
+  
+  my $dbhost = $db_conf{'dbhost'} || '127.0.0.1';
+  my $dbport = $db_conf{'dbport'} || '3306';
+  my $database = $db_conf{'database'} || 'eminfo';
+  my $dbuser = $db_conf{'dbuser'} || 'eminfo';
+  my $dbpass = $db_conf{'dbpass'} || 'eminfo';
+  my $dbcharset = $db_conf{'$dbcharset'} || 'utf8';
+  ### $dbhost
+  ### $dbport
+  ### $database
+  ### $dbuser
+  ### $dbpass
+  ### $dbcharset
+
+  my $result = '';
+
+  my ($connect, $sql);
+  $connect = DBI->connect(
+	"DBI:mysql:database=$database;host=$dbhost;port=$dbport",
+	"$dbuser",
+	"$dbpass",
+	{ RaiseError => 0, PrintError => 0, AutoCommit => 1 },
+  ); 
+  unless ($connect) {
+	print "connect mysql error: [",DBI->errstr,"]\n";
+	return undef;
+  }
+
+  unless ($connect->do("set names $dbcharset;")) {
+	print "set names $dbcharset error: [",DBI->errstr,"]\n";
+	return undef;
+  }
+
+  if ($table eq 'postlog') {
+  	$sql=$connect->prepare("select count(*) from $table where id='$eminfo_id' and plugin='$plugin';");
+  } elsif ($table eq 'heartbeat') {
+	$sql=$connect->prepare("select count(*) from $table where id='$eminfo_id';");
+  }
+  unless ($sql->execute()) {
+	print "sql error: [",DBI->errstr,"]\n";
+	return undef;
+  }
+
+  my $num=($sql->fetchrow_array())[0];
+  ### $num
+
+  if ($num >= 1) {  # update
+	if ($table eq 'postlog') {
+		$sql=$connect->prepare("update $table set content='$text', time=UNIX_TIMESTAMP() where id='$eminfo_id' and plugin='$plugin';");
+	} elsif ($table eq 'heartbeat') {
+		$sql=$connect->prepare("update $table set content='$text', time=UNIX_TIMESTAMP() where id='$eminfo_id';");
+	}
+  } else {	    # insert
+	if ($table eq 'postlog') {
+		$sql=$connect->prepare("insert $table (id,time,plugin,content) values ('$eminfo_id',UNIX_TIMESTAMP(),'$plugin','$text');");
+	} elsif ($table eq 'heartbeat') {
+		$sql=$connect->prepare("insert $table (id,time,content) values ('$eminfo_id',UNIX_TIMESTAMP(),'$text');");
+	}
+  }
+  unless ($sql->execute()) {
+	print "sql error: [",DBI->errstr,"]\n";
+	return undef;
+  }
+
+  $sql->finish();
+  $connect->disconnect();
+
+}
+
+1;
---
+++ sql/eminfo.sql
@@ -0,0 +1,82 @@
+--
+-- Database: `eminfo`
+--
+
+SET NAMES utf8;
+USE eminfo;
+-- GRANT SELECT,INSERT,UPDATE ON eminfo.* to eminfo@127.0.0.1 IDENTIFIED BY 'eminfo';
+-- FLUSH PRIVILEGES;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dbversion`
+--
+DROP TABLE IF EXISTS `dbversion`;
+SET character_set_client = utf8;
+CREATE TABLE IF NOT EXISTS `dbversion` (
+  `name` varchar(30) NOT NULL,		-- dbname
+  `version` varchar(30) NOT NULL 	-- dbversion
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `heartbeat`
+--
+DROP TABLE IF EXISTS `heartbeat`;
+SET character_set_client = utf8;
+CREATE TABLE IF NOT EXISTS `heartbeat` (
+  `id` varchar(10) NOT NULL,		-- eminfo id
+  `time` int(11) NOT NULL,		-- upate time
+  `content` varchar(128) NOT NULL,  	-- palmus content
+  PRIMARY KEY (`id`)			-- primary key
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `postlog`
+--
+DROP TABLE IF EXISTS `postlog`;
+SET character_set_client = utf8;
+CREATE TABLE IF NOT EXISTS `postlog` (
+  `id` varchar(10) NOT NULL,		-- eminfo id
+  `time` int(11) NOT NULL,		-- update time
+  `plugin` varchar(64) NOT NULL,  	-- plugin name
+  `content` text NOT NULL,		-- xml output
+  PRIMARY KEY (`id`,`plugin`),		-- primary key
+  KEY `ik_0` (`id`)			-- index
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `hosts`
+--
+DROP TABLE IF EXISTS `hosts`;
+SET character_set_client = utf8;
+CREATE TABLE IF NOT EXISTS `hosts` (
+  `id` varchar(10) NOT NULL,            -- eminfo id   (trigger from: postlog.id)
+  `name` varchar(64) NOT NULL,		-- eminfo name (trigger from: postlog.plugin)
+  `alias` varchar(64) NOT NULL,		-- eminfo alias (set by administrator)
+  PRIMARY KEY (`id`)			-- primary key
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `plugins`
+--
+DROP TABLE IF EXISTS `plugins`;
+SET character_set_client = utf8;
+CREATE TABLE IF NOT EXISTS `plugins` (
+  `id` varchar(10) NOT NULL,            -- eminfo id   (trigger from: postlog.id)
+  `plugin` varchar(64) NOT NULL,	-- plugin name (trigger from: postlog.plugin)
+  PRIMARY KEY (`id`)			-- primary key
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `host_groups`
+--
+
+--
+-- Table structure for table `host_group_members`
+--
+
+--
+-- Table structure for table `plugin_configs`
+--
